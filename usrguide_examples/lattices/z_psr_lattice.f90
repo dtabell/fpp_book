1c97605f9b9b679a55acc245e47e88535139f7ce
Dan T. Abell <dabell69@earthlink.net>
2015-04-07 20:48:25 -0600

    Added subroutine build_full_PSR(), which includes optional arguments for using rbends rather than sbends, and for turning on the sextupoles

--- usrguide_examples/lattices/z_psr_lattice.f90
+++ usrguide_examples/lattices/z_psr_lattice.f90
@@ -41,6 +41,93 @@ end subroutine build_PSR
 
 
 !=================================================================
+! Build the *full* PSR lattice as described in the paper "Exact
+! numerical calculation of chromaticity in small rings" by Alex J.
+! Dragt: Part. Accel. 12:205--218 (1982).
+!
+subroutine  build_full_PSR(PSR, useRBends, sextupolesON)
+use madx_ptc_module
+use pointer_lattice
+implicit none
+
+type(layout), target :: PSR
+logical(lp), intent(in), optional :: useRBends
+logical(lp), intent(in), optional :: sextupolesON
+
+logical(lp) :: useRB, sextON
+real(dp) :: ang, brho, kd, kf, Larc
+type(fibre) :: b, d1, d2, qd, qf
+type(layout) :: cell0, cellT, cellL
+!-----------------------------------
+
+useRB = .false.
+if (present(useRBends)) then
+  useRB = useRBends
+endif
+
+sextON = .false.
+if (present(sextupolesON)) then
+  sextON = sextupolesON
+endif
+
+call make_states(.false.)
+exact_model = .true.
+!default = default + nocavity + exactmis
+default = default + nocavity
+call update_states
+madlength = .false.
+
+ang = (twopi * 36.d0 / 360.d0)
+Larc = 2.54948d0
+brho = 1.2d0 * (Larc / ang)
+call set_mad(brho = brho, method = 2, step = 10)
+madkind2 = drift_kick_drift
+
+if (useRB) then
+  kqd = -1.92d0 / brho
+  kqf =  2.72d0 / brho
+  ksd = -3.50d0 / brho
+  ksf =  1.70d0 / brho
+else
+  kqd = -2.68d0 / brho
+  kqf =  1.95d0 / brho
+  ksd = -2.40d0 / brho
+  ksf =  1.60d0 / brho
+endif
+
+d1 = drift("D1", 2.28646d0)
+d2 = drift("D2", 0.45d0)
+d3 = drift("D3", 1.48646d0)
+d4 = drift("D4", 0.30d0)
+qf = quadrupole("QF", 0.5d0, kqf)
+qd = quadrupole("QD", 0.5d0, kqd)
+sf = quadrupole("SF", 0.5d0, ksf)
+sd = quadrupole("SD", 0.5d0, ksd)
+if (useRB) then
+  b  = rbend("B", Larc, ang)
+else
+  b  = sbend("B", Larc, ang)
+endif
+
+! lattice period with no sextupole
+cell0 = d1 + qd + d2 + b + d2 + qf + d1
+! lattice period with a trailing sextupole
+cellT = d1 + qd + d2 + b + d2 + qf + d4 + sf + d3
+! lattice period with a leading sextupole
+cellL = d3 + sd + d4 + qd + d2 + b + d2 + qf + d1
+
+if (sextON) then
+  PSR = cell0 + cellT + cellL + 3 * cell0 + cellT + cellL  + 2 * cell0
+else
+  PSR = 10 * cell0
+endif
+PSR = .ring.PSR
+
+call survey(PSR)
+end subroutine build_full_PSR
+
+
+!=================================================================
 
 subroutine  build_PSR_minus(PSR)
 use madx_ptc_module
